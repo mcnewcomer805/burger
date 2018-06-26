8ca8af5cdba6f21a54527033a54e9bbc397a194e
Michael Newcomer <mcnewcomer@hotmail.com>
2018-06-25 23:47:29 -0700

    added burgers

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
+
+DESCRIBE burgers;
+
+SELECT * FROM burgers;
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('avacado', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('theWorks', true);
+
+
